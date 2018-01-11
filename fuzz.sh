04ab6b810a694b4c960a01475bcc67e0fabcdc78
omair2084 <omairmo@outlook.com>
2018-01-11 17:05:47 +0530

    Update fuzz.sh

--- fuzz.sh
+++ fuzz.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export TERM=xterm-color
 
-cp -f /mruby/bin/mruby /results/mruby
+#cp -f /mruby/bin/mruby /results/mruby
 
 echo "Starting MRuby fuzzing with $(nproc) cores/fuzzers"
 
@@ -11,22 +11,9 @@ AFL_OUT_DIR=/results
 
 # Start master and slaves in background and hide any output.
 echo "Starting master-fuzzer"
-afl-fuzz -i $TESTCASE_DIR -o $AFL_OUT_DIR -t 10000 -Mmaster-0 -- /results/mruby @@ >/dev/null &
+gnome-terminal -- afl-fuzz -i $TESTCASE_DIR -o $AFL_OUT_DIR -t 10000 -m none -Mmaster-0 /mruby/bin/mruby
 for i in $(seq 1 $SLAVE_COUNT); do
     echo "Starting slave $i"
-    afl-fuzz -i $TESTCASE_DIR -o $AFL_OUT_DIR -t 10000 -Sslave-$i -- /results/mruby @@ >/dev/null &
+    gnome-terminal -- afl-fuzz -i $TESTCASE_DIR -o $AFL_OUT_DIR -t 10000 -m none -Sslave-$i /mruby/bin/mruby
     sleep 1
-done
-
-echo "Done, starting status screen!"
-
-# Loop afl-whatsup so that statistics can be viewed and container does not exit.
-# Also run crashwalk so that new crashes will be triaged automatically.
-cd /results
-while true; do
-  clear && echo && date && echo && echo
-  afl-whatsup -s /results
-  sleep 5
-  /root/gopath/bin/cwtriage -workers $(nproc) -tidy -afl -root .
-  /root/gopath/bin/cwdump ./crashwalk.db > triage.txt
 done
